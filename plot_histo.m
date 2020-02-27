e349599e96f8ae90e5b8844633795aa1eb6bb1be
Tazio <a.giusti@selecta.ve.it>
2020-02-27 10:57:55 +0100

    with norm

--- plot_histo.m
+++ plot_histo.m
@@ -6,8 +6,6 @@ function plot_histo(folder)
     max_Values = importdata(load_factor_max_csv);
     std_values = std(max_Values);
     mean_values = mean(max_Values);
-    disp(std_values);
-    disp(mean_values);
     max_Values = round(max_Values,1);
     mean_str = num2str(mean_values);
     std_str = num2str(std_values);
@@ -16,7 +14,19 @@ function plot_histo(folder)
     annotation('textbox',dim,'String',text,'FitBoxToText','on');
     histogram(max_Values);
     norm=histfit(max_Values,10, 'normal');
+    xlabel('Load Factor');
+    ylabel('Relative frequency [%]');
+    title('Load factor frequency distribution');
     disp(norm);
-%     hold on;
-%     plot(max_Values, p);
+    figure(3);
+    new_mean = 1; % scegli la nuona media.
+    new_std = 0.15; % scegli la nuova std dev.
+    traslation_values = new_mean + (max_Values - mean_values) * (new_std / std_values) ;
+    text = {['Std = ' num2str(round(new_std, 2))],['Mean = ' num2str(round(new_mean, 2))]};
+    annotation('textbox',dim,'String',text,'FitBoxToText','on');
+    h = histfit(traslation_values);
+    delete(h(1));
+    xlabel('Load Factor');
+    ylabel('Relative frequency [%]');
+    title('Normalized load factor frequency distribution');
     hold off;
